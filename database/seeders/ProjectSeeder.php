e302e48a41329be98977de4983d67e38f4bfe331
Benedetto-Bonaccorso <BenedettoBonaccorso00@gmail.com>
2023-01-10 14:52:11 +0100

    commit-iniziale-tardivo

---
+++ database/seeders/ProjectSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Project;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+
+class ProjectSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        for ($i=0; $i < 10 ; $i++) { 
+            $project = new Project();
+            $project->title = $faker->text(100);
+            $project->author = $faker->name();
+            $project->deadline = $faker->date();
+            $project->save();
+        }
+    }
+}
